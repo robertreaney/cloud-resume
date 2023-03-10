fd96b5222a02313546ed8ed65a5a0d7a78d924cd
Robert <robertlreaney@gmail.com>
2023-03-10 17:34:22 -0500

    test github update

--- pipeline.py
+++ pipeline.py
@@ -5,5 +5,3 @@ client = boto3.client('s3')
 
 print('updating index.html...')
 response = client.upload_file('website/index.html', 'reaney-resume', 'index.html',ExtraArgs={'ContentType':'text/html'})
-
-response.json()
