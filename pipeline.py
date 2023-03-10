854f2c456f57d9a6e467d036a83644951edac3d9
Robert <robertlreaney@gmail.com>
2023-03-10 16:34:39 -0500

    get failure for aws sync

--- pipeline.py
+++ pipeline.py
@@ -1,10 +1,9 @@
-# import boto3
+import boto3
 
-# client = boto3.client('s3')
+print('connecting to s3 bucket...')
+client = boto3.client('s3')
 
-# response = client.upload_file('website/index.html', 'reaney-resume', 'index.html',ExtraArgs={'ContentType':'text/html'})
+print('updating index.html...')
+response = client.upload_file('website/index.html', 'reaney-resume', 'index.html',ExtraArgs={'ContentType':'text/html'})
 
-# response = client.upload_file('run.py', 'reaney-resume', 'run.py')
-# response.json()
-
-print('hello from python')
+response.json()
