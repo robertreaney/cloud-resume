07c6ffd2cd11985b85d05eefccbb3ff845094331
Robert <robertlreaney@gmail.com>
2023-03-10 16:27:50 -0500

    test python runner

---
+++ pipeline.py
@@ -0,0 +1,10 @@
+# import boto3
+
+# client = boto3.client('s3')
+
+# response = client.upload_file('website/index.html', 'reaney-resume', 'index.html',ExtraArgs={'ContentType':'text/html'})
+
+# response = client.upload_file('run.py', 'reaney-resume', 'run.py')
+# response.json()
+
+print('hello from python')
