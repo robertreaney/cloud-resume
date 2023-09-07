86278fcb1f3e0f88fce33aac80c6334655675860
Robert <robertlreaney@gmail.com>
2023-09-07 19:53:13 -0400

    test ci

--- scripts/refresh-ec2.sh
+++ scripts/refresh-ec2.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+ssh -i ~/.ssh/id_rsa ubuntu@52.54.162.179 -o StrictHostKeyChecking=no "bash -s" << EOF
+#!/bin/bash
+
 date +"%Y-%m-%d %H:%M:%S" > ~/refresh-proof.log
 cd cloud-resume
 git pull
 docker compose up -d --build
+EOF
