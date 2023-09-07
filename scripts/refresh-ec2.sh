5e08f78b4ac2d90954a39b8a10f2fe324282fe12
Robert <robertlreaney@gmail.com>
2023-09-07 19:48:07 -0400

    pushing ci/cd changes

--- scripts/refresh-ec2.sh
+++ scripts/refresh-ec2.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-ssh -i ~/.ssh/id_rsa ubuntu@52.54.162.179 -o StrictHostKeyChecking=no "bash -s" << EOF
-#!/bin/bash
-
 date +"%Y-%m-%d %H:%M:%S" > ~/refresh-proof.log
-git clone https://github.com/robertreaney/cloud-resume.git
-EOF
+cd cloud-resume
+git pull
+docker compose up -d --build
