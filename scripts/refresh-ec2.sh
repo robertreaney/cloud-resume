02e0d74e60437532c429c83f6d06cd8666a85578
robertreaney <63528440+robertreaney@users.noreply.github.com>
2023-09-07 20:59:54 -0400

    Cd (#7)
    * test ci
    
    * test cd
    
    * wrong path
    
    * wrong script used
    
    * added correct perms to ci script
    
    * add log debugging
    
    * trying to catch failure in an output somewhere
    
    * again trying to find ssh failure
    
    * debugging ssh auth failure
    
    * fixed environment variable used for ssh paste
    
    * added elevation to docker command in ec2
    
    * corrected to prod deploy in cd action
    
    * finished cd ec2 delivery DEV mode compose up
    
    * switched to dev mode until https solved
    
    * trying to fix http prod deployment

--- scripts/refresh-ec2.sh
+++ scripts/refresh-ec2.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+ssh -v -i ~/.ssh/id_rsa ubuntu@52.54.162.179 -o StrictHostKeyChecking=no "bash -s" << EOF
+#!/bin/bash
+
 date +"%Y-%m-%d %H:%M:%S" > ~/refresh-proof.log
 cd cloud-resume
+git checkout main
 git pull
-docker compose up -d --build
+sudo docker compose -f docker-compose.yml -f docker-compose.prod.yml up --build -d
+EOF
