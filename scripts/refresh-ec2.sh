6ba4d7124b85d2a956cf7baf2fa0874d1886b578
Robert <robertlreaney@gmail.com>
2023-09-07 20:41:20 -0400

    corrected to prod deploy in cd action

--- scripts/refresh-ec2.sh
+++ scripts/refresh-ec2.sh
@@ -6,5 +6,5 @@ ssh -v -i ~/.ssh/id_rsa ubuntu@52.54.162.179 -o StrictHostKeyChecking=no "bash -
 date +"%Y-%m-%d %H:%M:%S" > ~/refresh-proof.log
 cd cloud-resume
 git pull
-sudo docker compose up -d --build
+sudo docker compose -f docker-compose.yml -f docker-compose.prod.yml up --build -d
 EOF
